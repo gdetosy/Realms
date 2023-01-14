3c76b4360f40300a37d5e61a4d9d99f15cbde4a0
gdetosy <tosyjdm@gmail.com>
2023-01-14 14:56:14 +0300

    1

---
+++ Realms/Realms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realms
+//
+//  Created by tosy on 14.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
