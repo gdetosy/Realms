4cddb6873288ec102b58c06dd144818931e5f0c3
gdetosy <tosyjdm@gmail.com>
2023-01-15 17:02:16 +0300

    1

--- Realms/Realms/Controllers/TasksTableViewController.swift
+++ Realms/Realms/Controllers/TasksTableViewController.swift
@@ -46,11 +46,12 @@ class TasksTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        title = currentTasksList?.name
+      
+        filteringTasks()
+        
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonSystemItemSelector))
+        self.navigationItem.setRightBarButtonItems([add, editButtonItem], animated: true)
     }
 
     // MARK: - Table view data source
@@ -60,10 +61,10 @@ class TasksTableViewController: UITableViewController {
         return 2
     }
 
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        section == 0 ? notCompletedTasks.count : completedTasks.count
-//
-//    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        section == 0 ? notCompletedTasks.count : completedTasks.count
+
+    }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         section == 0 ? "Not completed tasks" : "Completed tasks"
