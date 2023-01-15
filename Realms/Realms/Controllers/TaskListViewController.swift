48aeb5e3fc801c6f053a10a6de199042349cf036
gdetosy <tosyjdm@gmail.com>
2023-01-15 14:57:16 +0300

    part2

--- Realms/Realms/Controllers/TaskListViewController.swift
+++ Realms/Realms/Controllers/TaskListViewController.swift
@@ -105,15 +105,17 @@ class TaskListViewController: UITableViewController {
      }
      */
     
-    /*
+
      // MARK: - Navigation
      
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationVC = segue.destination as? TasksTableViewController,
+           let index = tableView.indexPathForSelectedRow {
+            let tasksList = tasksLists[index.row]
+            destinationVC.currentTasksList = tasksList
+        }
+    }
+  
     @objc func addBarButtonItemSelector() {
         alertForTask {
             [weak self] in print("new list add")
